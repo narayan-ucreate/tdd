becd6a9f2ec7ed9555e6e1b1dee3a66b61fce16a
Narayan Sharma <narayansharma@ucreate.co.in>
2019-06-18 12:40:42 +0530

    configuration changes

--- models/index.js
+++ models/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+console.log('this is final env')
+console.log(env)
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
